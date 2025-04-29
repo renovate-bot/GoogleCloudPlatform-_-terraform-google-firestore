6855bb92300d55de996682c71e7a79bec983fa28
Gautam04 <gautanar@google.com>
2025-04-29 15:21:58 +0530

    chore: Restricted permissions for integration tests (#9)

--- test/setup/iam.tf
+++ test/setup/iam.tf
@@ -16,7 +16,11 @@
 
 locals {
   int_required_roles = [
-    "roles/owner"
+    "roles/datastore.owner",
+    "roles/cloudkms.admin",
+    "roles/iam.serviceAccountAdmin",
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/resourcemanager.projectIamAdmin"
   ]
 }
 
